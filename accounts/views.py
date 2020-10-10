688302d3eee808b62af4355d936acfffdb052195
Romain Testard <romain.testard@bluenove.com>
2020-10-10 11:40:45 +0200

    I have implemented data queries between views and templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,28 @@
+from .models import *
 from django.shortcuts import render
 
+
 def home(request):
-   return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders, 'customers': customers,
+               'total_orders': total_orders, 'delivered': delivered,
+               'pending': pending}
+
+    return render(request, 'accounts/dashboard.html', context)
+
 
 def products(request):
-   return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+
+    return render(request, 'accounts/products.html', {'products': products})
 
 
 def customer(request):
